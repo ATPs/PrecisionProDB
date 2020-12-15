6160eca6e915c5fe07a33844faa3e1d4033f2176
Xiaolong Cao <ATPs@users.noreply.github.com>
2020-12-15 15:02:45 -0500

    add support for UniProt

--- src/perGeno.py
+++ src/perGeno.py
@@ -1,20 +1,21 @@
 from perGeno_core import PerGeno
 from perGeno_vcf import runPerGenoVCF
 import os
+import downloadhuman
 
 
 description = '''
 PerGeno, personal proteogenomic tools which outputs a new reference protein based on the variants data. 
 VCF/tsv file as the variant input
-if the variant file is in tsv format, at list four columns are required: chr, pos, ref, alt.
+If the variant file is in tsv format, at least four columns are required in the header: chr, pos, ref, alt. Try to convert the file to proper format if you have a bed or other file.
 '''
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('-g','--genome', help = 'the reference genome sequence in fasta format. It can be a gzip file', required=True)
-    parser.add_argument('-f', '--gtf', help='gtf file with CDS and exon annotations. It can be a gzip file', required=True)
+    parser.add_argument('-g','--genome', help = 'the reference genome sequence in fasta format. It can be a gzip file', default='')
+    parser.add_argument('-f', '--gtf', help='gtf file with CDS and exon annotations. It can be a gzip file', default='')
     parser.add_argument('-m', '--mutations', help='''a file stores the variants. If the file ends with ".vcf" or ".vcf.gz", treat as vcf input. Otherwise, treat as TSV input ''', required=True)
-    parser.add_argument('-p','--protein', help = 'protein sequences in fasta format. It can be a gzip file. Only proteins in this file will be checked', required=True)
+    parser.add_argument('-p','--protein', help = 'protein sequences in fasta format. It can be a gzip file. Only proteins in this file will be checked', default='')
     parser.add_argument('-t', '--threads', help='number of threads/CPUs to run the program. default, use all CPUs available', type=int, default=os.cpu_count())
     parser.add_argument('-o', '--out', help='''output prefix. Five files will be saved, including the annotation for mutated transcripts, the mutated or all protein sequences, two variant files from vcf. {out}.pergeno.aa_mutations.csv, {out}.pergeno.protein_all.fa, {out}.protein_changed.fa, {out}.vcf2mutation_1/2.tsv. default "perGeno" ''', default="perGeno")
     parser.add_argument('-a', '--datatype', help='''input datatype, could be GENCODE_GTF, GENCODE_GFF3, RefSeq, Ensembl_GTF or gtf. default "gtf". perGeno does not support Ensembl GFF3 ''', default='gtf', type=str, choices=['GENCODE_GTF', 'GENCODE_GFF3','RefSeq','Ensembl_GTF','gtf'])
@@ -22,6 +23,9 @@ if __name__ == '__main__':
     parser.add_argument('-F', '--no_filter', help='default only keep variant with value "PASS" FILTER column of vcf file. if set, do not filter', action='store_true')
     parser.add_argument('-s', '--sample', help='sample name in the vcf to extract the variant information. default: None, extract the first sample', default=None)
     parser.add_argument('-A','--all_chromosomes', help='default keep variant in chromosomes and ignore those in short fragments of the genome. if set, use all chromosomes including fragments when parsing the vcf file', action='store_true')
+    parser.add_argument('-D','--download', help='''download could be 'GENCODE','RefSeq','Ensembl','Uniprot'. If set, perGeno will try to download genome, gtf and protein files from the Internet. Download will be skipped if "--genome, --gtf, --protein, (--uniprot)" were all set. Settings from "--genome, --gtf, --protein, (--uniprot), --datatype" will not be used if the files were downloaded by perGeno. default "".''', default='', type=str, choices=['GENCODE','RefSeq','Ensembl','Uniprot',''])
+    parser.add_argument('-U','--uniprot', help='''uniprot protein sequences. If more than one file, use "," to join the files. default "". For example, "UP000005640_9606.fasta.gz", or "UP000005640_9606.fasta.gz,UP000005640_9606_additional.fasta" ''', default='', type=str)
+    
     f = parser.parse_args()
     
     file_genome = f.genome
@@ -35,7 +39,48 @@ if __name__ == '__main__':
     filter_PASS = not f.no_filter
     individual = f.sample
     chromosome_only = not f.all_chromosomes
+    download = f.download
+    files_uniprot = f.uniprot
     print(f)
+
+    # create workfolder if not exist
+    workfolder = os.path.dirname(outprefix)
+    if workfolder == '':
+        workfolder = '.'
+    if workfolder != '.':
+        if not os.path.exists(workfolder):
+            os.makedirs(workfolder)
+
+    # download required files if download is set
+    if download != '':
+        print('-D --download is set to be', download, '\n')
+        download = download.upper()
+        
+        if download != 'UNIPROT':
+            if file_genome != '' and file_gtf != '' and file_protein != '':
+                print('download already finished for', download)
+                pass
+            else:
+                files_downloaded = downloadhuman.download(download, workfolder=workfolder)
+                file_genome, file_gtf, file_protein = files_downloaded
+                if download == 'GENCODE':
+                    datatype = 'GENCODE_GTF'
+                elif download == 'REFSEQ':
+                    datatype = 'RefSeq'
+                elif download == 'ENSEMBL':
+                    datatype = 'Ensembl_GTF'
+                else:
+                    print('download is', download, 'which is not possible...')
+        else:
+            if file_genome != '' and file_gtf != '' and file_protein != '' and files_uniprot != '':
+                print('download already finished for', download)
+                pass
+            else:
+                file_genome, file_gtf, file_protein, file_uniprot, file_uniprot_additional = files_downloaded
+                files_uniprot = ','.join([file_uniprot, file_uniprot_additional])
+                datatype = 'Ensembl_GTF'
+
+
 
     if file_mutations.lower().endswith('.vcf') or file_mutations.lower().endswith('.vcf.gz'):
         print('variant file is a vcf file')
@@ -47,3 +92,12 @@ if __name__ == '__main__':
         pergeno.splitInputByChromosomes()
         #print(pergeno.__dict__)
         pergeno.runPerChom()
+    
+    # deal with uniprot
+    if download == 'UNIPROT':
+        print('try to extract Uniprot proteins from Ensembl models')
+        import extractmutateduniprot
+        extractmutateduniprot.extractMutatedUniprot(files_uniprot=files_uniprot, files_ref=file_protein, files_alt=outprefix + '.pergeno.protein_all.fa', outprefix=outprefix, length_min = 20)
+
+    
+    print('perGeno finished!')
