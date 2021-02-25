e64c9db3629cff4b64e05ef4ee02e70da4582e40
Xiaolong Cao <ATPs@users.noreply.github.com>
2021-02-24 23:40:05 -0500

    add PEFF option

--- src/PrecisionProDB.py
+++ src/PrecisionProDB.py
@@ -27,7 +27,8 @@ if __name__ == '__main__':
     parser.add_argument('-D','--download', help='''download could be 'GENCODE','RefSeq','Ensembl','Uniprot'. If set, perGeno will try to download genome, gtf and protein files from the Internet. Download will be skipped if "--genome, --gtf, --protein, (--uniprot)" were all set. Settings from "--genome, --gtf, --protein, (--uniprot), --datatype" will not be used if the files were downloaded by perGeno. default "".''', default='', type=str, choices=['GENCODE','RefSeq','Ensembl','Uniprot',''])
     parser.add_argument('-U','--uniprot', help='''uniprot protein sequences. If more than one file, use "," to join the files. default "". For example, "UP000005640_9606.fasta.gz", or "UP000005640_9606.fasta.gz,UP000005640_9606_additional.fasta" ''', default='', type=str)
     parser.add_argument('--uniprot_min_len', help='''minimum length required when matching uniprot sequences to proteins annotated in the genome. default 20 ''', default=20, type=int)
-    
+    parser.add_argument('--PEFF', help='whether output a PEFF format file. Default: False', action='store_true')
+
     
     f = parser.parse_args()
     
@@ -105,5 +106,12 @@ if __name__ == '__main__':
         import extractMutatedUniprot
         extractMutatedUniprot.extractMutatedUniprot(files_uniprot=files_uniprot, files_ref=file_protein, files_alt=outprefix + '.pergeno.protein_all.fa', outprefix=outprefix, length_min = uniprot_min_len)
 
-    
+    # generate PEFF output file
+    if f.PEFF:
+        import generatePEFFoutput
+        generatePEFFoutput.generatePEFFoutput(file_protein = file_protein, file_mutation = outprefix + '.pergeno.aa_mutations.csv', file_out = outprefix + '.pergeno.protein_PEFF.fa', TEST=False)
+        if download == 'UNIPROT':
+            generatePEFFoutput.generateUniprotPEFFout(file_PEFF = outprefix + '.pergeno.protein_PEFF.fa', files_uniprot_ref = files_uniprot, file_uniprot_changed = outprefix + '.uniprot_changed.tsv', file_uniprot_out = outprefix + '.uniprot_PEFF.fa')
+
+
     print('PrecisionProDB finished! Total seconds:', time.time() - time0)
