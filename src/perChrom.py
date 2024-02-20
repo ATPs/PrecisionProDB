1c3395babec6446b229c5c29223562d7d628d01e
Xiaolong Cao <ATPs@users.noreply.github.com>
2024-02-20 13:29:55 +0800

    fix a bug

--- src/perChrom.py
+++ src/perChrom.py
@@ -520,7 +520,6 @@ class PerChrom(object):
             print('frame shift, complex case, no change for', transcript_id)
             return {}
 
-        if codons_alt['AA_index'].isnull()
         codons_alt.loc[codons_alt.duplicated('AA_index'),['codon_ref','AA_ref','AA_index']] = np.NaN # only use each AA_ref Once
         codons_alt['AA_alt'] = codons_alt.apply(lambda x:x['AA_ref'] if x['codon_alt'] == x['codon_ref'] else str(Seq(x['codon_alt']).translate()), axis=1)
         codons_alt['AA_index'] = codons_alt['AA_index'].fillna(method='ffill')
