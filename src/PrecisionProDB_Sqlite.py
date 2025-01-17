d4021a1b0059a728ec0863510ea90f2a98902140
Xiaolong Cao <ATPs@users.noreply.github.com>
2025-01-18 01:12:23 +0800

    Update PrecisionProDB_Sqlite.py

--- src/PrecisionProDB_Sqlite.py
+++ src/PrecisionProDB_Sqlite.py
@@ -38,8 +38,9 @@ if TEST:
     file_sqlite = '/XCLabServer002_fastIO/examples/GENCODE/GENCODE.tsv.sqlite'
 
 
-def runSinglePerChromSqlite(file_sqlite, file_mutations, tempfolder, threads, chromosome, datatype):
+def runSinglePerChromSqlite(file_sqlite, file_mutations, tempfolder, threads, chromosome, datatype, individual):
     '''
+    run PerChrom_sqlite for a single chromosome
     '''
     outprefix = os.path.join(tempfolder, chromosome)
     print(file_mutations)
@@ -48,7 +49,8 @@ def runSinglePerChromSqlite(file_sqlite, file_mutations, tempfolder, threads, ch
                     threads = threads,
                     outprefix = outprefix,
                     datatype = datatype,
-                    chromosome = chromosome)
+                    chromosome = chromosome,
+                    individual = individual)
     print('run perchrom_sqlite for chromosome', chromosome)
     # perchrom_sqlite.run_perChrom()
     # print('finished running perchrom_sqlite for chromosome:', chromosome)
@@ -62,7 +64,7 @@ def runSinglePerChromSqlite(file_sqlite, file_mutations, tempfolder, threads, ch
         return None
 
 
-def runPerChomSqlite(file_sqlite, file_mutations, threads, outprefix, protein_keyword, datatype, keep_all, chromosomes_genome, chromosomes_genome_description, file_gtf):
+def runPerChomSqlite(file_sqlite, file_mutations, threads, outprefix, protein_keyword, datatype, keep_all, individual, chromosomes_genome, chromosomes_genome_description, file_gtf):
     '''
     '''
     # split file_mutations by chromosome
@@ -78,12 +80,14 @@ def runPerChomSqlite(file_sqlite, file_mutations, threads, outprefix, protein_ke
         keep_all = keep_all
         )
     
+    if individual == 'ALL_VARIANTS':
+        individual = ''
     tempfolder = pergeno.tempfolder
     pergeno.chromosomes_genome = chromosomes_genome
     chromosomes_mutation = pergeno.splitMutationByChromosome(chromosomes_genome_description=chromosomes_genome_description, chromosomes_genome=chromosomes_genome)
 
     # run runSinglePerChromSqlite
-    chromosomes_mutated = [runSinglePerChromSqlite(file_sqlite, f'{tempfolder}/{chromosome}.mutation.tsv', tempfolder, threads, chromosome, datatype) for chromosome in chromosomes_mutation]
+    chromosomes_mutated = [runSinglePerChromSqlite(file_sqlite, f'{tempfolder}/{chromosome}.mutation.tsv', tempfolder, threads, chromosome, datatype, individual) for chromosome in chromosomes_mutation]
     # successful chromosomes
     chromosomes_mutated = [e for e in chromosomes_mutated if e is not None]
     # collect mutation annotations
@@ -109,7 +113,7 @@ def runPerChomSqlite(file_sqlite, file_mutations, threads, outprefix, protein_ke
                 proteins_changed_ids.append(s.id)
                 fout_proteins_changed.write('>{}\n{}\n'.format(s.description, str(s.seq)))
         
-    proteins_changed_ids = set(proteins_changed_ids)
+    proteins_changed_ids = set([i.split('__')[0] for i in proteins_changed_ids])
     conn = buildSqlite.get_connection(file_sqlite)
     df_protein_description = pd.read_sql("SELECT * FROM protein_description", conn)
     df_protein_description = df_protein_description[~ df_protein_description['protein_id_fasta'].isin(proteins_changed_ids)]
@@ -133,121 +137,33 @@ def runPerChomSqlite(file_sqlite, file_mutations, threads, outprefix, protein_ke
 def runPerChomSqlite_vcf(file_mutations, file_sqlite, threads, outprefix, datatype, protein_keyword, keep_all, individual, chromosome_only, filter_PASS, chromosomes_genome, chromosomes_genome_description, file_gtf):
     '''
     '''
-    from vcf2mutation import getMutationsFromVCF
+    from vcf2mutation import convertVCF2MutationComplex
     from PrecisionProDB_vcf import readProtein2DF, openFile
     # get two mutation files from vcf file
     print('start extracting mutation file from the vcf input')
     outprefix_vcf = outprefix + '.vcf2mutation'
-    getMutationsFromVCF(file_vcf = file_mutations, outprefix = outprefix_vcf, individual=individual, filter_PASS = filter_PASS, chromosome_only = chromosome_only)
+    individual = convertVCF2MutationComplex(file_vcf = file_mutations, outprefix = outprefix_vcf, individual=individual, filter_PASS = filter_PASS, chromosome_only = chromosome_only)
+    individual = ','.join(individual)
     print('finished extracting mutations from the vcf file')
-    file_mutations_1 = outprefix_vcf + '_1.tsv'
-    file_mutations_2 = outprefix_vcf + '_2.tsv'
-
-    # run perGeno for mutations_1
-    print('start running PrecisionProDB for first strand of the genome mutation file')
-    outprefix_1 = outprefix + '_1'
-    runPerChomSqlite(
-            file_mutations = file_mutations_1, 
-            file_sqlite = file_sqlite,
-            threads=threads, 
-            outprefix=outprefix_1, 
-            datatype=datatype, 
-            protein_keyword=protein_keyword, 
-            keep_all=keep_all,
-            chromosomes_genome = chromosomes_genome, 
-            chromosomes_genome_description = chromosomes_genome_description,
-            file_gtf=file_gtf
-            )
+    file_mutations = outprefix + '.vcf2mutation.tsv'
+    
+    if TEST:
+        print([file_sqlite, file_mutations, threads, outprefix, protein_keyword, datatype, keep_all, individual, chromosomes_genome, chromosomes_genome_description, file_gtf])
+    
+    runPerChomSqlite(file_sqlite, 
+                     file_mutations, 
+                     threads, 
+                     outprefix, 
+                     protein_keyword, 
+                     datatype, 
+                     keep_all, 
+                     individual, 
+                     chromosomes_genome, 
+                     chromosomes_genome_description, 
+                     file_gtf
+                     )
     
 
-    # run perGeno for mutations_2
-    print('start running PrecisionProDB for second strand of the genome mutation file')
-    outprefix_2 = outprefix + '_2'
-    runPerChomSqlite(
-            file_mutations = file_mutations_2, 
-            file_sqlite = file_sqlite,
-            threads=threads, 
-            outprefix=outprefix_2, 
-            datatype=datatype, 
-            protein_keyword=protein_keyword, 
-            keep_all=keep_all,
-            chromosomes_genome = chromosomes_genome, 
-            chromosomes_genome_description = chromosomes_genome_description,
-            file_gtf=file_gtf
-            )
-    # read in the changed protein sequences
-    fout_protein_changed_1 = outprefix_1 + '.pergeno.protein_changed.fa'
-    fout_protein_changed_2 = outprefix_2 + '.pergeno.protein_changed.fa'
-    fout_protein_changed = outprefix +  '.pergeno.protein_changed.fa'
-    df_seqs_changed_1 = readProtein2DF(fout_protein_changed_1)
-    df_seqs_changed_2 = readProtein2DF(fout_protein_changed_2)
-    df_seqs_changed_1['batch'] = '1'
-    df_seqs_changed_2['batch'] = '2'
-    df_seqs_changed = pd.concat([df_seqs_changed_1,df_seqs_changed_2], ignore_index=True)
-
-    # read in the mutation files
-    fout_mutations_1 = outprefix_1 + '.pergeno.aa_mutations.csv'
-    fout_mutations_2 = outprefix_2 + '.pergeno.aa_mutations.csv'
-    fout_mutations = outprefix + '.pergeno.aa_mutations.csv'
-    df_mutations_1 = pd.read_csv(fout_mutations_1, sep='\t')
-    df_mutations_2 = pd.read_csv(fout_mutations_2, sep='\t')
-    df_mutations_1['batch'] = '1'
-    df_mutations_2['batch'] = '2'
-    df_mutations = pd.concat([df_mutations_1, df_mutations_2], ignore_index=True)
-
-    # join the mutation and protein file
-    df_joined = df_seqs_changed.merge(df_mutations, left_on = ['seq_id','batch'], right_on = ['protein_id_fasta','batch'])
-
-    df_joined_dedup = df_joined.fillna('')
-    df_joined_dedup = df_joined_dedup.groupby(by = [e for e in df_joined_dedup.columns if e != 'batch'])['batch'].apply(lambda x:';'.join(str(e) for e in x)).reset_index()
-
-    print('total number of changed proteins from two strands of chromosome: {}. After removing protein seq_id with the same mutations, {} sequences left. After remove protein seq_id with same final sequence, {} sequences left.'.format(df_seqs_changed_1.shape[0] + df_seqs_changed_2.shape[0], df_joined_dedup.shape[0], df_joined.groupby(['seq_id','seq']).ngroups))
-
-    protein_ids_changed = set(df_joined_dedup['protein_id_fasta'])
-    protein_ids_changedBothStrands = set(df_seqs_changed_1['seq_id']) & set(df_seqs_changed_2['seq_id'])
-    n_changed_both = len(protein_ids_changedBothStrands)
-    n_changed_bothDiffMut = df_joined_dedup.shape[0] - len(protein_ids_changed)
-    print('proteins changed in at least one strand: {}. Proteins changed in both strands: {}. Proteins changed in both strands and the mutation in both strands are identical: {}. Proteins changed in both strands, the final protein sequences are identical but the mutations are different: {}'.format(len(protein_ids_changed), n_changed_both, n_changed_both - n_changed_bothDiffMut, df_joined_dedup.shape[0] - df_joined.groupby(['seq_id','seq']).ngroups))
-    
-    ## save the result
-    df_joined_dedup['seq_id_final'] = df_joined_dedup.apply(lambda r:r['seq_id'] + '__'+ r['batch'] if r['batch'] in ['1','2'] else r['seq_id'] +'__12',axis=1)
-    f = open(fout_protein_changed,'w')
-    for _, r in df_joined_dedup.iterrows():
-        f.write('>{}\t{}\n{}\n'.format(r['seq_id_final'], r['seq_anno'], r['seq']))
-    f.close()
-
-    # combine all protein_sequences
-    fout_protein_all_1 = outprefix_1 + '.pergeno.protein_all.fa'
-    fout_protein_all_2 = outprefix_2 + '.pergeno.protein_all.fa'
-    fout_protein_all = outprefix + '.pergeno.protein_all.fa'
-    f = open(fout_protein_all,'w')
-
-    conn = buildSqlite.get_connection(file_sqlite)
-    df_protein_description = pd.read_sql("SELECT * FROM protein_description", conn)
-    df_protein_description = df_protein_description[~ df_protein_description['protein_id_fasta'].isin(protein_ids_changedBothStrands)]
-    for _, s in df_protein_description.iterrows():
-        protein_id, protein_description, protein_id_fasta, AA_seq = s['protein_id'], s['protein_description'], s['protein_id_fasta'], s['AA_seq']
-        f.write('>{}\tunchanged\n{}\n'.format(protein_description, str(AA_seq)))
-    
-    conn.close()
-    
-    f.write(open(fout_protein_changed).read())
-    f.close()
-
-
-    df_mutations = df_joined_dedup[df_mutations_1.columns].copy()
-    df_mutations['protein_id_fasta'] = df_joined_dedup['seq_id_final']
-    df_mutations.to_csv(fout_mutations, sep='\t',index=None)
-
-    # clean up files
-    if not keep_all:
-        os.remove(fout_mutations_1)
-        os.remove(fout_mutations_2)
-        os.remove(fout_protein_changed_1)
-        os.remove(fout_protein_changed_2)
-        os.remove(fout_protein_all_1)
-        os.remove(fout_protein_all_2)
-        os.removedirs(outprefix + '_temp')
     print('perGeno_vcf finished!')
 
 
@@ -306,6 +222,9 @@ def check_sqlite_file(file_path):
             conn.close()
             
 def main_PrecsionProDB_Sqlite(file_genome, file_gtf, file_mutations, file_protein, threads, outprefix, datatype, protein_keyword, filter_PASS, individual, chromosome_only, keep_all, file_sqlite):
+    '''
+    the main function of PrecisionProDB_Sqlite
+    '''
 
     if os.path.exists(file_sqlite):
         if not check_sqlite_file(file_sqlite):
@@ -396,6 +315,7 @@ def main_PrecsionProDB_Sqlite(file_genome, file_gtf, file_mutations, file_protei
             datatype=datatype, 
             protein_keyword=protein_keyword, 
             keep_all=keep_all,
+            individual=individual,
             chromosomes_genome=chromosomes_genome,
             chromosomes_genome_description=chromosomes_genome_description,
             file_gtf=file_gtf
@@ -414,14 +334,25 @@ def main():
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-g','--genome', help = 'the reference genome sequence in fasta format. It can be a gzip file', default='')
     parser.add_argument('-f', '--gtf', help='gtf file with CDS and exon annotations. It can be a gzip file', default='')
-    parser.add_argument('-m', '--mutations', help='''a file stores the variants. If the file ends with ".vcf" or ".vcf.gz", treat as vcf input. Otherwise, treat as TSV input. a string like "chr1-788418-CAG-C" can used as variant input, too. ''', default = '', required=False)
+    parser.add_argument('-m', '--mutations', help='''
+                        a file stores the variants. 
+                        If the file ends with ".vcf" or ".vcf.gz", treat as vcf input. Otherwise, treat as TSV input. 
+                        a string like "chr1-788418-CAG-C" or "chr1-942451-T-C,1-6253878-C-T,1-2194700-C-G" can used as variant input, too. In this mode, --sample will not be used.
+                        If multiple vcf files are provided, use "," to join the file names. For example, "--mutations file1.vcf,file2.vcf". A pattern match is also supported for input vcf, but quote is required to get it work. For example '--mutations "file*.vcf" ' 
+                        
+                        ''', default = '', required=False)
     parser.add_argument('-p','--protein', help = 'protein sequences in fasta format. It can be a gzip file. Only proteins in this file will be checked', default='')
     parser.add_argument('-t', '--threads', help='number of threads/CPUs to run the program. default, use all CPUs available', type=int, default=min(20, os.cpu_count()))
     parser.add_argument('-o', '--out', help='''output prefix, folder path could be included. Three or five files will be saved depending on the variant file format. Outputs include the annotation for mutated transcripts, the mutated or all protein sequences, two variant files from vcf. {out}.pergeno.aa_mutations.csv, {out}.pergeno.protein_all.fa, {out}.protein_changed.fa, {out}.vcf2mutation_1/2.tsv. default "perGeno" ''', default="perGeno")
     parser.add_argument('-a', '--datatype', help='''input datatype, could be GENCODE_GTF, GENCODE_GFF3, RefSeq, Ensembl_GTF or gtf. default "gtf". Ensembl_GFF3 is not supported. ''', default='gtf', type=str, choices=['GENCODE_GTF', 'GENCODE_GFF3','RefSeq','Ensembl_GTF','gtf'])
     parser.add_argument('-k','--protein_keyword', help='''field name in attribute column of gtf file to determine ids for proteins. default "auto", determine the protein_keyword based on datatype. "transcript_id" for GENCODE_GTF, "protein_id" for "RefSeq" and "Parent" for gtf and GENCODE_GFF3 ''', default='auto')
     parser.add_argument('-F', '--no_filter', help='default only keep variant with value "PASS" FILTER column of vcf file. if set, do not filter', action='store_true')
-    parser.add_argument('-s', '--sample', help='sample name in the vcf to extract the variant information. default: None, extract the first sample. ', default=None)
+    parser.add_argument('-s', '--sample', help='''
+                        sample name in the vcf to extract the variant information. default: None, extract the first sample. 
+                        For multiple samples, use "," to join the sample names. For example, "--sample sample1,sample2,sample3". 
+                        To use all samples, use "--sample ALL_SAMPLES". 
+                        To use all variants regardless where the variants from, use "--sample ALL_VARIANTS".
+                        ''', default=None)
     parser.add_argument('-A','--all_chromosomes', help='default keep variant in chromosomes and ignore those in short fragments of the genome. if set, use all chromosomes including fragments when parsing the vcf file', action='store_true')
     parser.add_argument('--keep_all', help='If set, do not delete files generated during the run', action='store_true')
     parser.add_argument('-S','--sqlite', help='''A path of sqlite file for re-use of annotation info. default '', do not use sqlite. The program will create a sqlite file if the file does not exist. If the file already exists, the program will use data stored in the file. It will cause error if the content in the sqlite file is not as expected. ''', default='', type=str)
@@ -447,7 +378,7 @@ def main():
     print(description)
     print(f)
 
-    main(file_genome, file_gtf, file_mutations, file_protein, threads, outprefix, datatype, protein_keyword, filter_PASS, individual, chromosome_only, keep_all, file_sqlite)
+    main_PrecsionProDB_Sqlite(file_genome, file_gtf, file_mutations, file_protein, threads, outprefix, datatype, protein_keyword, filter_PASS, individual, chromosome_only, keep_all, file_sqlite)
 
 if __name__ == '__main__':
     main()
